4608e0f1fcd27b82086d295a363519ebb7b0b27e
user <user@192.168.10.106>
2018-06-11 20:00:29 +0900

    プロパティ取得ストアド作成

--- master/stored/get_bng_mstr_001.sql
+++ master/stored/get_bng_mstr_001.sql
@@ -38,14 +38,83 @@ BEGIN
         SET @query = CONCAT("
             SELECT
                 BNG_NO
+                ,USR_NUM
                 ,BG_US_FLG
-                ,BG_URL
+                ,ITEM_NUM
+                ,URL_ITEM_NUM
                 ,PRFMNC_FLG
                 ,END_MSRE_FLG
                 ,PRBBLTY_FLG
                 ,PRBBLTY_CD
+                -- ユーザー情報プロパティ
+
+                -- 項目情報プロパティ
             FROM
-                T_BNG_MSTR
+                T_BNG_MSTR GP
+                -- ユーザー情報
+                ,(
+                    SELECT
+                        ,BNG_NO
+                        ,COUNT(1) AS USR_NUM
+                        ,GROUP_CONCAT(USR_CD) AS USR_CD
+                        ,GROUP_CONCAT(USR_NAME) AS USR_NAME
+                        ,GROUP_CONCAT(LCH_NUM) AS LCH_NUM
+                        ,GROUP_CONCAT(BNG_NUM) AS BNG_NUM
+                        ,GROUP_CONCAT(END_FLG) AS END_FLG
+                    FROM
+                        T_USR
+                    WHERE
+                        BNG_NO = ",_bng_no,"
+                    AND
+                        EXST_FLG = "1"
+                    GROUP BY
+                        BNG_NO
+                ) USR
+                -- 項目情報
+                ,(
+                    SELECT
+                        TBCM.BNG_NO
+                        ,GROUP_CONCAT(TBCM.ITEM_CD)
+                        ,GROUP_CONCAT(TBCM.ITEM_NAME)
+                        ,GROUP_CONCAY(TBCM.SH_NAME)
+                        ,GROUP_CONCAT(TBCM.PRBBLTY)
+                        ,GROUP_CONCAT(TU.ITEM_CD) AS CHSED_ITEM
+                        ,GROUP_CONCAT(COUNT(TBCM.ITEM_CD))) AS CHSED_ITEM_NUM
+                        ,GROUP_CONCAT(
+                            CASE
+                                WHEN TU.ITEM_CD ISNULL THEN NULL
+                                WHEN TU.ITEM_CD ISNOTNULL THEN TBCM.ITEM_NAME
+                                ELSE 99
+                            END
+                        ) AS CHSED_ITEM_NAME
+                        ,GROUP_CONCAT(
+                            CASE
+                                WHEN TU.ITEM_CD ISNULL THEN NULL
+                                WHEN TU.ITEM_CD ISNOTNULL THEN TBCM.SH_NAME
+                                ELSE 99
+                            END
+                        ) AS CHSED_SH_NAME
+                        ,GROUP_CONCAT(
+                            SUM(
+                                CASE
+                                    WHEN TU.ITEM_CD IS NOT NULL THEN 1
+                                    ELSE 0
+                                END
+                            )
+                        ) AS ALL_ITEM_NUM
+                    FROM
+                        T_BNG_CNTNT_MSTR TBCM
+                    LEFT OUTER JOIN
+                        T_USR TU
+                    ON
+                        TU.BNG_NO = TBCM.BNG_NO
+                    AND
+                        TU.ITEM_CD = TBCM.ITEM_CD
+                    WHERE
+                        TBCM.BNG_NO = ",_bnng_no,"
+                    GROUP BY
+                        TBCM.BNG_NO
+                ) IP
             WHERE
                 BNG_NO = ",_bng_no,"
         ")
