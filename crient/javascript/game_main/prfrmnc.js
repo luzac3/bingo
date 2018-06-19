4580e88c8a41bfbfcb714448023538776de05695
user <user@192.168.10.106>
2018-06-19 19:56:52 +0900

    演出追加

--- crient/javascript/game_main/prfrmnc.js
+++ crient/javascript/game_main/prfrmnc.js
@@ -2,29 +2,30 @@
  * ヒットアニメーション表示/リーチ判定/点滅処理
  * ビンゴ/リーチ演出
  */
-function prfrmnc(game_property){
-    let animLoop = new AnimLoop();
+async function prfrmnc(game_property){
+    return new Promise(function(){
+        let animLoop = new AnimLoop();
 
-    let property = {
-        obj:game_property
-        ,wrapper:game_property
-        ,canvas:canvas_name
-        ,repeat:null
-        ,operation_time:null
-        ,draw_interval:null
-    }
-    // ヒットアニメーション表示
-    animLoop.animloop(property,hit_anim);
+        let property = {
+            obj:game_property
+            ,wrapper:game_property
+            ,canvas:canvas_name
+            ,repeat:null
+            ,operation_time:null
+            ,draw_interval:null
+        }
+        // ヒットアニメーション表示
+        // animLoop.animloop(property,hit_anim);
 
-    // マス塗りつぶし処理
+        // マス塗りつぶし処理
+        gmae_proeprty = await animLoop.animloop(property,msre_hit_anim);
 
-    // リーチ増加フラグがたっていたらリーチ演出
+        // リーチ増加フラグがたっていたらリーチ演出
 
-    // ビンゴ増加フラグが立っていたらリーチ演出
+        // ビンゴ増加フラグが立っていたらリーチ演出
 
-
-
-    resolve(game_property);
+        return game_property;
+    })
 }
 
 /*
@@ -73,7 +74,8 @@ function msre_hit_anim(ctx,property){
             });
         }
         flash();
-    });}
+    });
+}
 
 /*
  * リーチ演出
