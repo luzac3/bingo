9f06641e16936d01d9fe746f597e38588a9e14d8
luzac3 <rey.silvy@gmail.com>
2018-06-21 21:31:15 +0900

    VIEW作成

--- sql/view/user_msre.sql
+++ sql/view/user_msre.sql
@@ -1,14 +1,34 @@
 -- ユーザーライン情報をまとめたVIEW
--- ライン番号、ビンゴ状態、リーチ状態を保持
-
--- ビンゴ番号、ライン番号、マス番号、ユーザコード、リーチ、ビンゴ状態フラグ
--- マス番号の数だけ生成されるため、
 
 CREATE VIEW V_USR_LN AS
     SELECT
-        BNG_NO
-        ,LN_NUM
-        ,USR_CD
-        ,LCH_FLG
-        ,BNG_FLG
+        LN.BNG_NO AS BNG_NO
+        ,LN.LN_NUM AS LN_NUM
+        ,LN.USR_CD AS USR_CD
+        ,CASE
+            WHEN LN.MSRE_NUM - LN.HIT_NUM = 1 THEN '1'
+            ELSE '0'
+        END AS LCH_FLG
+        ,CASE
+            WHEN LN.MSRE_NUM - LN.HIT_NUM = 0 THEN '1'
+            ELSE '0'
+        END AS BNG_FLG
     FROM
+        (
+            SELECT
+                TLM.BNG_NO AS BNG_NO
+                ,TLM.LN_NUM AS LN_NUM
+                ,TUM.USR_CD AS USR_CD
+                ,COUNT(TUM.CLL_FLG) AS HIT_NUM
+                ,COUNT(1) AS MSRE_NUM
+            FROM
+                T_LN_MSTR TLM
+            LEFT OUTER JOIN
+                T_USR_MSRE TUM
+            ON
+                TLM.BNG_NO = TUM.BNG_NO
+            GROUP BY
+                TLM.BNG_NO
+                ,TUM.USR_CD
+                ,TUM.CLL_FLG
+        ) LN
