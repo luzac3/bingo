25344faa17aa01b0de67286f9d982b81724c43d2
user <user@192.168.10.106>
2018-06-22 23:48:48 +0900

    ヘッダ整理

--- entrance/javascript/index.js
+++ entrance/javascript/index.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     $("button").on("click",function(){
-        let button_val =~ $(this).val();
+        let button_val = $(this).val();
         switch(button_val){
-          case register:
+          case "register":
             // 登録画面に遷移
             location.href = "/bingo/environment/html/register.html"
             break;
-          case join:
+          case "join":
             // クライアントページに遷移
             location.href = "/bingo/crient/html/main.html";
             break;
-          case master:
+          case "master":
               // ゲーム一覧画面に遷移→マスタではなくエントランス？
               // マスタはあくまでゲーム開始後の画面を想定
               location.href = "/bingo/entrance/html/game_list.html";
