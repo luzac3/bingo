dd01c03ba473c3290c75e968128acd2112749b1b
luzac3 <rey.silvy@gmail.com>
2018-07-02 22:56:53 +0900

    アイテム取得修正

--- sql/stored/get_item_list_001.sql
+++ sql/stored/get_item_list_001.sql
@@ -4,7 +4,7 @@ DELIMITER //
 -- get_item_list_001 項目リスト取得処理
 --
 -- 【処理概要】
---  アイテムの更新を待機する処理
+--  項目リストを条件に従い取得する処理
 --
 --
 -- 【呼び出し元画面】
@@ -12,7 +12,6 @@ DELIMITER //
 --
 -- 【引数】
 --   _bng_no              ：ビンゴ番号
---   _user_cd             ：ユーザコード
 --   _search_word         ：検索文字列
 --   _tag                 ：タグ名
 --
@@ -27,90 +26,105 @@ DELIMITER //
 -- ********************************************************************************************
 CREATE PROCEDURE `get_item_list_001`(
     IN `_bng_no` CHAR(5)
-    , IN `_user_cd` CHAR(5)
-    , IN `_search_word` VARCHAR(40)
     , IN `_tag` VARCHAR(200)
+    , IN `_search_word1` VARCHAR(40)
+    , IN `_search_word2` VARCHAR(40)
+    , IN `_search_word3` VARCHAR(40)
     , OUT `exit_cd` INTEGER
 )
-COMMENT '更新待機処理'
+COMMENT '項目リスト取得処理'
 
 BEGIN
 
     -- 異常終了ハンドラ
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION SET exit_cd = 99;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @errno = MYSQL_ERRNO, @text = MESSAGE_TEXT;
+        SELECT @sqlstate, @errno, @text;
+        ROLLBACK;
+        SET exit_cd = 99;
+    END;
 
-    SET @query = CONCAT("
-        SELECT
-            BNG_NO
-            ,MSRE_NUM
-            ,ITM_CD
-            ,ITM_NAME
-            ,ITM_SH_NAME
+    SET @query_before = CONCAT("
+        SELECT DISTINCT
+            TBI.BNG_NO
+            ,TBI.ITM_CD
+            ,TBI.ITM_NAME
+            ,TBI.ITM_SH_NAME
+            ,TBI.PRBBLLTY
+            ,TBI.IMG_URL
+            ,TBI.END_FLG
+            ,TBI.CHSD_NUM
+            ,TBI.KUSN_NYU_CD
         FROM
-            T_ITM
-        WHERE
-            BNG_NO = '",_bng_no,"'
+            T_BNG_ITM TBI
     ");
 
-    IF (IFNULL(_user_cd,0)) = 0 THEN
-        SET @query = CONCAT(
-            @query
-            ,""
-
-        -- ユーザーマスの呼び出し済みフラグをアップデート
-        UPDATE
-            T_USR_MSRE
-        SET
-            CLL_FLG = 1
-            ,KUSN_NTJ = NOW()
+    SET @query_after = CONCAT("
         WHERE
-            BNG_NO = _bng_no
-        AND
-            CNTNT_ID IN (
-                SELECT DISTINCT
-                    CNTNT_ID
-                FROM
-                    T_BNG_ITM
-                WHERE
-                    BNG_NO = _bng_no
-                AND
-                    KUSN_NYU_CD = "2"
-            )
-        ;
+            TBI.BNG_NO = '",_bng_no,"'
+    ");
 
-        SET @query = CONCAT("
-            -- VIEWと結合してリーチ、ビンゴ数を取得
-            SELECT
-                TBI.ITM_CD ITM_CD
-                ,USR_LN.LCH_NUM LCH_NUM
-                ,USR_LN.BNG_NUM BNG_NUM
-                ,MAX(KUSN_NTJ) AS KUSN_NTJ
-            FROM
-                T_BNG_ITM TBI
-                ,(
-                    SELECT
-                        COUNT (IF LCH_FLG = 1 THEN 1 ELSE NULL) AS LCH_NUM
-                        COUNT (IF BNG_FLG = 1 THEN 1 ELSE NULL) AS BNG_NUM
-                    FROM
-                        V_USR_LN
-                    WHERE
-                        USR_CD = ",_usr_cd,"
-                    GROUP BY
-                        BNG_NO
-                        ,LN_NUM
-                ) USR_LN
-            WHERE
-                TBI.BNG_NO = ",_bng_no,"
+    IF IFNULL(_tag,'') != '' THEN
+        SET @query_tag = CONCAT("
+            LEFT OUTER JOIN
+                T_BNG_TAG TBT
+            ON
+                TBI.BNG_NO = TBT.BNG_NO
             AND
-                TBI.KUSN_NYU_CD = '2'
-            GROUP BY
-                ITM_CD
-                ,LCH_NUM
-                ,BNG_NUM
-        ")
-        ;
+                TBI.ITM_CD = TBT.ITM_CD
+            AND
+                TBT.BNG_TAG IN (",_tag,")
+        ");
+    ELSE
+        SET @query_tag = "";
+    END IF;
 
-    SET @query_text = @query;
+    IF IFNULL(_search_word1,'') != '' THEN
+        SET @query_search = CONCAT("
+            AND(
+                (
+                        TBI.ITM_NAME LIKE ('%",_search_word1,"%')
+                    OR
+                        TBI.ITM_SH_NAME LIKE ('%",_search_word1,"%')
+                )
+        ");
+    END IF;
+
+    IF IFNULL(_search_word2,'') != '' THEN
+        SET @query_search = CONCAT(
+            @query_search,"
+                OR(
+                        TBI.ITM_NAME LIKE ('%",_search_word2,"%')
+                    OR
+                        TBI.ITM_SH_NAME LIKE ('%",_search_word2,"%')
+                )
+        ");
+    END IF;
+
+    IF IFNULL(_search_word3,'') != '' THEN
+        SET @query_search = CONCAT(
+            @query_search,"
+                OR(
+                        TBI.ITM_NAME LIKE ('%",_search_word3,"%')
+                    OR
+                        TBI.ITM_SH_NAME LIKE ('%",_search_word3,"%')
+                )
+            )
+        ");
+    ELSE
+        SET @query_search = CONCAT(
+            @query_search,"
+            )
+        ");
+    END IF;
+
+    IF IFNULL(_search_word1,'') = '' THEN
+        SET @query_search = "";
+    END IF;
+
+
+    SET @query_text = CONCAT(@query_before,@query_tag,@query_after,@query_search,";");
 
     -- 実行
     PREPARE main_query FROM @query_text;
